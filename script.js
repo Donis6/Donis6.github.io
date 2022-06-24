bdec32fdff5c899c0d34f21771a7e7a7a15efaac
Pollodonis6 <donisguillen@gmail.com>
2022-06-24 13:23:04 -0400

    javascript adeed

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function clickButton() {
     console.log("hi");
 }
+function showTools(){
+    console.log("")
+    var toolPics = document.getElementsByClassName("toolPics")
+    toolPics.style.visibility = "visible"
+    console.log(toolPics)
+}
